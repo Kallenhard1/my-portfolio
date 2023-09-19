8bba841a4753103cb8bcf3c4cb10db22e641aac9
Mario Lucas da Silva Barbosa <30977081+Kallenhard1@users.noreply.github.com>
2023-09-19 22:43:29 +0000

    refactoring...

--- pages/Hero.js
+++ pages/Hero.js
@@ -7,7 +7,13 @@ import styles from "./page.module.css";
 const Hero = () => {
   return (
     <div>
-      <nav className="hero-section">Hero</nav>
+      <nav className="hero-section">
+        <h2>LOGO</h2>
+        <h3>HOME</h3>
+        <h3>ABOUT</h3>
+        <h3>PROJECTS</h3>
+        <h3>CONTACT</h3>
+      </nav>
     </div>
   );
 };
