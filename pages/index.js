98609b9eec89e735fc106ee5f9ca61d414590df1
Mario Lucas da Silva Barbosa <30977081+Kallenhard1@users.noreply.github.com>
2023-09-22 15:04:02 +0000

    Added: `Body` to pages and added `Hero`, `Body` and `Contact` to `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import React from "react";
+
+import Hero from "./Hero.js";
+import Contact from "./Contact.js";
+import Body from "./Body.js";
+
 function Home() {
-  return <h1>Luana, Eu Te Amo! Se você me ama, da uma risadinha</h1>;
+  return (
+    <div>
+      <h1>Luana, Eu Te Amo! Se você me ama, da uma risadinha</h1>
+      <p>------------------------------</p>
+      <Hero />
+      <Body />
+      <p>------------------------------</p>
+      <Contact />
+    </div>
+  );
 }
 
 export default Home;
