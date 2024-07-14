13fce60b14fa091d7396c9817a6fcaea19598b5c
Mario Lucas da Silva Barbosa <30977081+Kallenhard1@users.noreply.github.com>
2024-07-14 15:31:27 +0000

    Adding eslint configs

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import styles from "./index.module.css";
 
 import Hero from "./Hero.js";
-import Contact from "./Contact.js";
 import Body from "./Body.js";
 
 function Home() {
